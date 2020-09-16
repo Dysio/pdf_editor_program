46fe156b1bd81f88a8987a67d8eeb4c3511c0060
Dysio <td.dyszewski@gmail.com>
2020-09-16 22:33:52 +0200

    update button remove last

--- pdf_app.py
+++ pdf_app.py
@@ -4,7 +4,7 @@ import os
 from pypdf2_functions import page_size_dict_func, split_pages
 
 root = tk.Tk()
-root.title('Dysio PDF size split')
+root.title('PDF size split')
 # root.iconbitmap('.icon\\pdf_icon_256x256.ico')
 
 files = []
@@ -30,12 +30,27 @@ def splitPdf():
         # os.startfile(file)
         split_pages(file, page_size_dict_func(file))
 
+def clear_list():
+    files.clear()
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+def remove_last():
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    files.pop(-1)
+
+    for file in files:
+        label = tk.Label(frame, text=file, bg="gray")
+        label.pack()
+
 
 canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.05)
+frame.place(relwidth=0.8, relheight=0.6, relx=0.1, rely=0.05)
 
 openFile = tk.Button(root, text="Open File", padx=10, pady=5,
                      fg="white", bg="#263D42", command=addPDF)
@@ -43,5 +58,11 @@ openFile.pack()
 
 splitPdf = tk.Button(root, text="Split PDF's", padx=10, pady=5, fg="white", bg="#263D42", command=splitPdf)
 splitPdf.pack()
+
+clearlist = tk.Button(root, text="Clear List", padx=10, pady=10, fg="white", bg="#263D42", command=clear_list)
+clearlist.pack()
+
+removelast = tk.Button(root, text="Remove Last", padx=10, pady=10, fg="white", bg="#263D42", command=remove_last)
+removelast.pack()
 
 root.mainloop()
